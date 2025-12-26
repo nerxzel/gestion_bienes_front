c4a592b94269e7560f843d55618c4f419f44afc6
Felipe Becerra <felipe.e.becerra@gmail.com>
2025-12-26 06:06:52 -0300

    renamed alta-baja components

--- src/features/alta-baja/pages/Alta.jsx
+++ src/features/alta-baja/pages/Alta.jsx
@@ -1,16 +1,16 @@
 import { Container, Card } from 'react-bootstrap';
-import BajaForm from '../components/BajaForm.jsx';
+import AltaForm from '../components/AltaForm.jsx';
 
-function ProcesosDarDeBaja() {
+function Alta() {
     return (
         <Container className="mt-5">
             <Card className="p-4 shadow-sm ">
                 <Card.Body>
-                    <BajaForm />
+                    <AltaForm />
                 </Card.Body>
             </Card>
         </Container>
     );
 }
 
-export default ProcesosDarDeBaja;
+export default Alta;
